b0178891c5796d7b7e9a723ea4d1e8b80f98a419
jarousselle <jarousselle@icloud.com>
2025-05-16 10:15:19 -0700

    changing colors in visualization to be different by latitude

--- test-script.R
+++ test-script.R
@@ -3,5 +3,8 @@ library(lterdatasampler)
 
 ggplot(data = pie_crab,
        aes(x = latitude,
-           y = size)) +
-  geom_point()
+           y = size,
+           color = latitude)) +
+  geom_point() +
+  scale_color_gradient(low = "#749243",
+                       high = "#386993")
